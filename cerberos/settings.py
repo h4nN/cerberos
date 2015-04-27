cc61f2e2388cefbe64345d6c79fedcdf62eae112
Nicco <niccolasmendoza@gmail.com>
2015-04-27 12:30:14 -0300

    Update settings.py

--- cerberos/settings.py
+++ cerberos/settings.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 
-MAX_FAILED_LOGINS = getattr(settings, 'MAX_FAILED_LOGINS', 3)
+MAX_FAILED_LOGINS = getattr(settings, 'MAX_FAILED_LOGINS', 5)
 
 # Number of seconds after the failed access attempts are forgotten.
 MEMORY_FOR_FAILED_LOGINS = getattr(settings, 'MEMORY_FOR_FAILED_LOGINS', 60)
 
-USERNAME_FIELD = 'username'
+USERNAME_FIELD = getattr(settings, 'USERNAME_FIELD', 'username')
